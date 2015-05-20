c722911692b1be926c5370729c73624a9556c19a
SHILPA SWETH <shilpasweth@gmail.com>
2015-05-20 19:22:28 +0400

    delta app 1

---
+++ clicker/app/src/main/java/com/shilpasweth/clicker/MainActivity.java
@@ -0,0 +1,68 @@
+package com.shilpasweth.clicker;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView myTextView=(TextView)
+                findViewById(R.id.textView);
+        if (savedInstanceState != null)   {
+            count=savedInstanceState.getInt("counter");
+            myTextView.setText(Integer.toString(count));
+    }
+
+}
+
+    @Override
+    protected void onSaveInstanceState(
+            Bundle savedInstanceState){
+        savedInstanceState.putInt(
+                "counter",count);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void buttonOnClick(View v) {
+        // do something when the button is clicked
+        Button button=(Button)v;
+        count++;
+        button.setText("I've Been Hit!");
+        TextView myTextView=(TextView)
+                findViewById(R.id.textView);
+        myTextView.setText(Integer.toString(count));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
